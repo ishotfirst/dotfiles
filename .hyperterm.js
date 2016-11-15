8c08255b5b540d810c94813a779b9e301ea9aadd
Jason LeMoine <github@lemoine.io>
2016-11-15 12:07:52 -0500

    iTerm prefs to Hyper

--- .hyperterm.js
+++ .hyperterm.js
@@ -4,19 +4,19 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Monaco, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: '#9e70c2',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
+    cursorShape: 'UNDERLINE',
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#f1f1f1',
 
     // terminal background color
-    backgroundColor: '#000',
+    backgroundColor: 'rgba(21,23,24,0.8)',
 
     // border color (window, tabs)
     borderColor: '#333',
@@ -27,6 +27,8 @@ module.exports = {
     // custom css to embed in the terminal window
     termCSS: '',
 
+    windowSize: '[120, 40]',
+
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
@@ -34,21 +36,21 @@ module.exports = {
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
+      black: '#414141',
+      red: '#cf3d41',
+      green: '#9ecb4d',
+      yellow: '#e7ce61',
+      blue: '#51b4dd',
+      magenta: '#9e70c2',
       cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
+      white: '#f1f1f1',
+      lightBlack: '#414141',
+      lightRed: '#cf3d41',
+      lightGreen: '#9ecb4d',
+      lightYellow: '#e7ce61',
+      lightBlue: '#51b4dd',
       lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
+      lightCyan: '#9ecb4d',
       lightWhite: '#ffffff'
     },
 
@@ -64,7 +66,7 @@ module.exports = {
     env: {},
 
     // set to false for no bell
-    bell: 'SOUND',
+    bell: false,
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false
